08773728ddbdbc5b989bdf6f0601bd44fc6cf9fb
Junior Damacena <you@example.com>
2023-01-07 16:20:22 -0300

    Refactoring the "Home" and "Favorite" pages

--- lib/pages/favorites_page.dart
+++ lib/pages/favorites_page.dart
@@ -1,4 +1,9 @@
+import 'package:english_words/english_words.dart';
+import 'package:fa_de_filme/widgets/movie_grid_tile.dart';
 import 'package:flutter/material.dart';
+
+import '../models/movie.dart';
+import 'details_page.dart';
 
 /// Página de filmes favoritos
 class FavoritesPage extends StatefulWidget {
@@ -9,22 +14,57 @@ class FavoritesPage extends StatefulWidget {
 }
 
 class _FavoritesPageState extends State<FavoritesPage> {
+  final _moviesSuggestions = <WordPair>[];
+
   @override
   Widget build(BuildContext context) {
+    var moviesList = Container(
+      color: Colors.deepPurple.withAlpha(220),
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.75,
+        ),
+        padding: const EdgeInsets.all(8.0),
+        itemBuilder: ((context, index) {
+          if (index >= _moviesSuggestions.length) {
+            _moviesSuggestions.addAll(generateWordPairs().take(10));
+          }
+
+          var name = _moviesSuggestions[index].asPascalCase;
+
+          var movieMock = Movie(
+              id: 99,
+              title: name,
+              releaseDate: "09/02/2022",
+              voteAverage: 6.556,
+              posterPath:
+                  "https://image.tmdb.org/t/p/w500/s16H6tpK2utvwDtzZ8Qy4qm5Emw.jpg",
+              overview:
+                  "12 anos depois de explorar Pandora e se juntar aos Na'vi, Jake Sully formou uma família com Neytiri e se estabeleceu entre os clãs do novo mundo. Porém, a paz não durará para sempre.",
+              runtime: 117);
+
+          return MovieGridTile(
+            movie: movieMock,
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) {
+                    return DetailsPage(movie: movieMock);
+                  },
+                ),
+              );
+            },
+          );
+        }),
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Favoritos"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Lista de filmes favoritos',
-            ),
-          ],
-        ),
-      ),
+      body: moviesList,
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:english_words/english_words.dart';
+import 'package:fa_de_filme/models/movie.dart';
 import 'package:fa_de_filme/pages/details_page.dart';
 import 'package:fa_de_filme/pages/favorites_page.dart';
+import 'package:fa_de_filme/widgets/movie_grid_tile.dart';
 import 'package:flutter/material.dart';
 
 /// Página inicial do app
@@ -15,7 +17,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final _moviesSuggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
 
   void _openFavorites() {
     Navigator.of(context).push(
@@ -28,48 +29,63 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: ListView.builder(
-            padding: const EdgeInsets.all(16.0),
-            // prototypeItem: ListTile(
-            //   title: Text(
-            //     "",
-            //     style: _biggerFont,
-            //   ),
-            // ),
-            itemBuilder: ((context, i) {
-              /* TODO the divider is conflicting with prototypeItem,
-                  it's getting it's height changed along with the other items */
-              if (i.isOdd) return const Divider();
-
-              final index = i ~/ 2;
+      body: NestedScrollView(
+        floatHeaderSlivers: true,
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              title: Text(widget.title),
+              pinned: false,
+              floating: true,
+              forceElevated: innerBoxIsScrolled,
+            ),
+          ];
+        },
+        body: Container(
+          color: Colors.deepPurple.withAlpha(220),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.75,
+            ),
+            padding: const EdgeInsets.all(8.0),
+            itemBuilder: ((context, index) {
               if (index >= _moviesSuggestions.length) {
                 _moviesSuggestions.addAll(generateWordPairs().take(10));
               }
 
               var name = _moviesSuggestions[index].asPascalCase;
 
-              return ListTile(
-                title: Text(
-                  name,
-                  style: _biggerFont,
-                ),
+              var movieMock = Movie(
+                  id: 99,
+                  title: name,
+                  releaseDate: "09/02/2022",
+                  voteAverage: 6.556,
+                  posterPath:
+                      "https://image.tmdb.org/t/p/w500/65NBNqJiaHeCmqDqkCmrRplJXw.jpg",
+                  overview:
+                      "Uma jovem extraordinária descobre seu superpoder e convoca a coragem notável, contra todas as probabilidades, para ajudar os outros a mudar suas histórias, ao mesmo tempo em que assume o controle de seu próprio destino. Defendendo o que é certo, ela obteve resultados milagrosos.",
+                  runtime: 117);
+
+              return MovieGridTile(
+                movie: movieMock,
                 onTap: () {
                   Navigator.of(context).push(
                     MaterialPageRoute(
-                      builder: (context) => DetailsPage(title: name),
+                      builder: (context) {
+                        return DetailsPage(movie: movieMock);
+                      },
                     ),
                   );
                 },
               );
-            })),
+            }),
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _openFavorites,
-        tooltip: 'Open Favorites',
+        tooltip: 'Abrir Favoritos',
         child: const Icon(Icons.bookmark),
       ),
     );
