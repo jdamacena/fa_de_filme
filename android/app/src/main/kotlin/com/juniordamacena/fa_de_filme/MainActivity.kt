47fd1664c8246ac0a51aca42227f570044af325f
Junior Damacena <juniordamacena980@gmail.com>
2023-02-07 20:37:22 -0300

    Fixes app not showing a preview in Android's recent apps screen

--- android/app/src/main/kotlin/com/juniordamacena/fa_de_filme/MainActivity.kt
+++ android/app/src/main/kotlin/com/juniordamacena/fa_de_filme/MainActivity.kt
@@ -3,4 +3,12 @@ package com.juniordamacena.fa_de_filme
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    protected override fun onPause() {
+        super.onPause()
+        try {
+            java.lang.Thread.sleep(200)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
 }
